7437025fa2333b3d08390abc4a40fab5b9ee0ed7
Eugene Skinner <eskinfrn@gmail.com>
2015-11-17 13:22:02 -0700

    Got a redirect working

--- js/controllers.js
+++ js/controllers.js
@@ -1,17 +1,18 @@
 app.controller('MainController',function($scope,$firebaseObject,$firebaseAuth,$location,$window){
   var ref = new Firebase("https://ghjsbusinesscard.firebaseio.com");
+  $scope.changeUrl = function(){
+    $location.path('/cardview');
+  };
 
   $scope.login = function(){
+    $location.path('/cardview');
     ref.authWithOAuthPopup("github", function(error, authData) {
       if (error) {
         console.log("Login Failed!", error);
       } else {
         console.log("Authenticated successfully with payload:", authData);
-        $window.location.href('/cardview');
       }
-
     });
-
   };
 });
 
