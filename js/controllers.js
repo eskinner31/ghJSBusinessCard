cd9d6e6e3f28c444bcf723a91d0768ff6501ad9e
Eugene Skinner <eskinfrn@gmail.com>
2015-11-21 12:27:13 -0700

    Able to request for public repos

--- js/controllers.js
+++ js/controllers.js
@@ -11,15 +11,34 @@ app.controller('MainController',function($scope,$firebaseObject,$firebaseAuth,$l
 
 });
 
-app.controller('CardController',function($scope,$firebaseObject,$firebaseAuth,workingAccount){
+app.controller('CardController',function($scope,$firebaseObject,$firebaseAuth,$http,workingAccount){
    var retrieveSession = workingAccount.retrieveSessionData;
    var setSession = workingAccount.set;
    var ref = new Firebase("https://ghjsbusinesscard.firebaseio.com");
    ref.onAuth(retrieveSession);
    var authData = ref.getAuth();
-   $scope.data = authData;
-   $scope.imageUrl = authData.github.profileImageURL;
-   $scope.name = authData.github.displayName;
-   $scope.userName = authData.github.username;
+   var currentUser = {
+     name : authData.github.displayName,
+     userName : authData.github.username,
+     imageUrl : authData.github.profileImageURL,
+     htmlUrl : authData.github.cachedUserProfile.html_url,
+     location : authData.github.cachedUserProfile.locaiton,
+     apiUrl : authData.github.cachedUserProfile.url,
+     key : authData.github.accessToken
+   };
+
+   $scope.getRepos = function(){
+     $http({
+       method:'GET',
+       url:'https://api.github.com/users/'+currentUser.userName+'/repos'
+     }).then(function(response){
+        console.log(response);
+     }, function(error){
+        console.log(error);
+     });
+   };
+
+   $scope.data = currentUser;
+
    console.log($scope.imageUrl);
 });
